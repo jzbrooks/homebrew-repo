f2f30e1014c179bd3c87ed8773d1b87e7421b0b9
Justin Brooks <justin@jzbrooks.com>
2023-11-13 21:06:42 -0500

    Fix the formula path

--- Formula/liturgist.rb
+++ Formula/liturgist.rb
@@ -1,15 +1,15 @@
 class Liturgist < Formula
   desc "A pdf generator for liturgical events"
   homepage "https://github.com/jzbrooks/liturgist"
-  url "https://github.com/jzbrooks/jzbrooks/releases/download/1.0/liturgist"
+  url "https://github.com/jzbrooks/liturgist/releases/download/1.0/liturgist.jar"
   version "1.0"
   sha256 "6b374451ab2a624cee178bcfa67ba2a7dd37ef489c0cc44c5905089782ef5a85"
 
   depends_on "openjdk"
 
   def install
-    libexec.install "liturgist"
-    bin.write_jar_script libexec/"liturgist", "liturgist"
+    libexec.install "liturgist.jar"
+    bin.write_jar_script libexec/"liturgist.jar", "liturgist"
   end
 
   test do
