768cc5ad36282e18610d5c284e8fdf793b6edc75
Justin Brooks <justin@jzbrooks.com>
2024-10-11 20:44:51 -0400

    Fix checksum

--- Formula/vgo.rb
+++ Formula/vgo.rb
@@ -2,7 +2,7 @@ class Vgo < Formula
   desc "A tool for optimizing and converting between vector artwork representations."
   url "https://github.com/jzbrooks/vgo/releases/download/v2.4.0/vgo"
   version "2.4.0"
-  sha256 "81acc293614ed57a0b93518283f9e8e3fb8c0e727ed1dd19c2fc848a643b29b6"
+  sha256 "ed9c8f4fcb9d56a2d53164563eca1d3efd5c659e32e2f29869cb126f77f03776"
 
   depends_on "openjdk"
 
