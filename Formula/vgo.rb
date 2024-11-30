52714e76c6e55c4dd53a40cb58574dac8279dd83
Justin Brooks <justin@jzbrooks.com>
2024-11-29 22:56:08 -0500

    Update vgo.rb

--- Formula/vgo.rb
+++ Formula/vgo.rb
@@ -1,7 +1,7 @@
 class Vgo < Formula
   desc "A tool for optimizing and converting between vector artwork representations."
   url "https://github.com/jzbrooks/vgo/releases/download/v3.0.0/vgo"
-  version "2.4.0"
+  version "3.0.0"
   sha256 "6fa9fb2635b1bf32136c1640625cc9f3390bc4bd779c6a4e478452f1232c5b57"
 
   depends_on "openjdk"
