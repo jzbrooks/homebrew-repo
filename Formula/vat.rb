8c6f1fdb27a57d14216b1d19cfba9d208abae117
Justin Brooks <justin@jzbrooks.com>
2026-01-23 18:33:56 -0800

    Update VAT to version 0.0.8

--- Formula/vat.rb
+++ Formula/vat.rb
@@ -1,8 +1,8 @@
 class Vat < Formula
   desc "A tool for rendering vector artwork to the terminal via kitty graphics protocol"
-  url "https://github.com/jzbrooks/vat/releases/download/0.0.7/vat-macos-arm64"
-  version "0.0.7"
-  sha256 "e07f19dda8f80140e656e4e387c885dce2f47119821c408754601ba756b13ae4"
+  url "https://github.com/jzbrooks/vat/releases/download/0.0.8/vat-macos-arm64"
+  version "0.0.8"
+  sha256 "b444dcb0308afd50155f1ad1a5030812d96a711cfb2f974dec10c49a856f79ce"
 
   depends_on "openjdk"
 
